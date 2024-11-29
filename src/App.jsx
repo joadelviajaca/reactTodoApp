513f3644a6a922bef36ada722a3aa506e4cc68fc
joadelvia <joadelvia@gmail.com>
2024-11-29 11:57:55 +0100

    Falta delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+
+const url = 'http://localhost:3000/todos';
 
 function App() {
 
   const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [add, setAdd] = useState(0);
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(todos => {
+        setTasks(todos);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+        setLoading(false); // Desactivar el indicador de carga en caso de error
+      })
+
+  }, [add])
 
   const handleChange = (e) => {
     setTask(e.target.value);
@@ -13,15 +31,30 @@ function App() {
   const handleDelete = (index) => {
     console.log('first')
     const newTasks = [...tasks];
-    newTasks.splice(index,1);
+    newTasks.splice(index, 1);
     setTasks(newTasks);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTasks([...tasks, task]);
-    setTask('');
+    if (task) {
+      
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({ name: task })
+      })
+        .then(response => response.json())
+        .then(task => {
+          setAdd(add + 1);
+          // setTasks([...tasks, task]);
+          setTask('');
+        }
+        )
+        .catch(error => console.log(error))
+    }
   }
+
+
 
   return (
     <>
@@ -31,13 +64,22 @@ function App() {
         <button onClick={handleSubmit}>Add</button>
 
       </form>
-      <ul>
-        { tasks.map((task,index) => (
-          <li key={index}>{task}
-            <button type='button' onClick={()=>handleDelete(index)}>X</button>
-          </li>
-        ))}
-      </ul>
+      {
+        loading ? (
+          <p>Cargando...</p>
+        ) :
+          (
+
+            <ul>
+              {tasks.map((task) => (
+                <li key={task.id}>{task.name}
+                  <button type='button' onClick={() => handleDelete(task.id)}>X</button>
+                </li>
+              ))}
+            </ul>
+          )
+
+      }
     </>
   )
 }
